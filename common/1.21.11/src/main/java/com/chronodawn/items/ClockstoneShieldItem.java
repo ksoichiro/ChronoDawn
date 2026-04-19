d34c315aead4282c40ffb637345524b0b069a02e
ksoichiro <soichiro.kashima@gmail.com>
2026-04-19 22:11:52 +0900

    feat(shields): add en_us/ja_jp translations and tooltip rendering

--- common/1.21.11/src/main/java/com/chronodawn/items/ClockstoneShieldItem.java
+++ common/1.21.11/src/main/java/com/chronodawn/items/ClockstoneShieldItem.java
@@ -2,7 +2,13 @@ package com.chronodawn.items;
 
 import com.chronodawn.items.shield.ChronoShieldMarker;
 import com.chronodawn.items.shield.ChronoShieldTier;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShieldItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+
+import java.util.function.Consumer;
 
 public class ClockstoneShieldItem extends ShieldItem implements ChronoShieldMarker {
     public ClockstoneShieldItem(Properties properties) {
@@ -12,5 +18,13 @@ public class ClockstoneShieldItem extends ShieldItem implements ChronoShieldMark
     @Override
     public ChronoShieldTier getChronoShieldTier() {
         return ChronoShieldTier.T1;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay tooltipDisplay,
+                               Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipDisplay, tooltipAdder, tooltipFlag);
+        tooltipAdder.accept(Component.translatable("item.chronodawn.clockstone_shield.tooltip.1"));
+        tooltipAdder.accept(Component.translatable("item.chronodawn.clockstone_shield.tooltip.2"));
     }
 }
--- common/1.21.11/src/main/java/com/chronodawn/items/EnhancedClockstoneShieldItem.java
+++ common/1.21.11/src/main/java/com/chronodawn/items/EnhancedClockstoneShieldItem.java
@@ -2,7 +2,13 @@ package com.chronodawn.items;
 
 import com.chronodawn.items.shield.ChronoShieldMarker;
 import com.chronodawn.items.shield.ChronoShieldTier;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShieldItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+
+import java.util.function.Consumer;
 
 public class EnhancedClockstoneShieldItem extends ShieldItem implements ChronoShieldMarker {
     public EnhancedClockstoneShieldItem(Properties properties) {
@@ -12,5 +18,13 @@ public class EnhancedClockstoneShieldItem extends ShieldItem implements ChronoSh
     @Override
     public ChronoShieldTier getChronoShieldTier() {
         return ChronoShieldTier.T2;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay tooltipDisplay,
+                               Consumer<Component> tooltipAdder, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipDisplay, tooltipAdder, tooltipFlag);
+        tooltipAdder.accept(Component.translatable("item.chronodawn.enhanced_clockstone_shield.tooltip.1"));
+        tooltipAdder.accept(Component.translatable("item.chronodawn.enhanced_clockstone_shield.tooltip.2"));
     }
 }
