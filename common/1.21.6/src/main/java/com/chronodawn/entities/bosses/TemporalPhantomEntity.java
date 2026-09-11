3352874c3a753de1dfcdaedd19407f666f4fffb9
ksoichiro <soichiro.kashima@gmail.com>
2026-09-11 23:09:43 +0900

    feat(bosses): Temporal Phantom switches target immediately when hit by a player

--- common/1.21.6/src/main/java/com/chronodawn/entities/bosses/TemporalPhantomEntity.java
+++ common/1.21.6/src/main/java/com/chronodawn/entities/bosses/TemporalPhantomEntity.java
@@ -115,6 +115,20 @@ public class TemporalPhantomEntity extends Monster implements RangedAttackMob {
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
+    /**
+     * Switch target to whoever just hit us, even if we already had a
+     * different target. Vanilla's HurtByTargetGoal only takes effect while
+     * the boss has no target, so without this override the boss keeps
+     * chasing the first player it saw while everyone else hits it for free.
+     */
+    @Override
+    public void setLastHurtByMob(LivingEntity entity) {
+        super.setLastHurtByMob(entity);
+        if (entity instanceof Player) {
+            this.setTarget(entity);
+        }
+    }
+
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
